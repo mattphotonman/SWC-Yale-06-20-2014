7160811bfecc3fd778e6570eff00699c9f297aef
Matt <a@b.c>
2014-06-20 15:22:48 -0400

    Back again to multi-column.

--- generate_data.sh
+++ generate_data.sh
@@ -1,5 +1,9 @@
 # generate data 1 - 10
+# this is fun
 
 for i in {1..10}; do
-    echo $i >> data1.txt
+    let j=i+1
+    echo $i $j >> data1.txt
 done
+awk '{print $1, $2^2}' data1.txt > tmp
+mv tmp data1.txt
