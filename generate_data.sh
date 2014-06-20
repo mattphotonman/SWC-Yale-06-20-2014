39e243d699b7434795dd9dc3a4fb8c70b6926a71
Matt <a@b.c>
2014-06-20 15:02:37 -0400

    Went back to old version of generate_data.sh

--- generate_data.sh
+++ generate_data.sh
@@ -1,9 +1,5 @@
 # generate data 1 - 10
-# this is fun
 
 for i in {1..10}; do
-    let j=i+1
-    echo $i $j >> data1.txt
+    echo $i >> data1.txt
 done
-awk '{print $1, $2^2}' data1.txt > tmp
-mv tmp data1.txt
