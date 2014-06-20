22f0d83926531972ddb400100fc3fc4eb3f0d15b
Matt <a@b.c>
2014-06-20 13:59:01 -0400

    Added i= into simple_for_loop.sh

--- generate_data.sh
+++ generate_data.sh
@@ -1,4 +1,5 @@
 # generate data 1 - 10
+# this is fun
 
 for i in {1..10}; do
     echo $i >> data1.txt
--- simple_for_loop.sh
+++ simple_for_loop.sh
@@ -1,5 +1,5 @@
 # loop to print 1 to 10
 
 for i in {1..10}; do
-    echo $i
+    echo "i = " $i
 done
