0399b709e3c4c03b17ce55a0f124f34fd69c5edf
Matt <a@b.c>
2014-06-21 16:19:40 -0400

    Sample plotting and fitting.

--- plot_my_data.py
+++ plot_my_data.py
@@ -10,7 +10,7 @@ def plot_in_line( myarray, figname='fig1' ) :
     plb.plot(myarray[0],myarray[1],c='r',ls=':')
     plb.savefig(figname) 
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
     '''Checks if we're just running plot_my_data.'''
     myarray = np.array([range(10),[i**2 for i in range(10)]])
     plot_in_line(myarray)
